90bb90f77d781fe4e257140343f9691915230fa4
BADimov21 <BADimov21@codingburgas.bg>
2022-12-10 21:16:49 +0200

    Add base of main menu,
    Co-authored-by: Bozhidar Dimov <BADimov21@codingburgas.bg>
    Co-authored-by: Gospodin Radev <GSRadev21@codingburgas.bg>

---
+++ main_menu.cpp
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+//Libraries which the code requires
+#include <iostream>
+#include <cwchar>
+#include <windows.h>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    
+    system("Color 0E");
+    cout << endl;
+    cout << setw(50) << "MathWord";
+    cout << endl;
+    cout << endl;
+    cout << "This game was made by JUST CODE" << setw(49) << "__" << endl;
+    cout << "                               " << setw(50) << "|_|" << endl;
+    cout << setw(31) << "|======================|" << setw(37) << " | |" << setw(10) << "| |" << endl;
+    cout << setw(31) << "|======================|" << setw(37) << " | |" << setw(10) << "| |" << endl;
+    cout << setw(30) << "| Choose what game   |" << setw(38) << " | |" << setw(10) << "| |" << endl;
+    cout << setw(30) << "| you want to play!  |" << setw(38) << " | |" << setw(10) << "| |" << endl;
+    cout << setw(30) << "|                    |" << setw(38) << " | |" << setw(10) << "| |" << endl;
+    cout << setw(30) << "| Hangman:           |" << setw(38) << " | |" << setw(10) << "| |" << endl;
+    cout << setw(30) << "| Enter 1            |" << setw(38) << " " << setw(9) << "" << endl;
+    cout << setw(30) << "|                    |" << setw(36) << " |" << setw(10) << "|" << endl;
+    cout << setw(30) << "| Bingo:             |" << endl;
+    cout << setw(30) << "| Enter 2            |" << endl;
+    cout << setw(30) << "|                    |" << setw(41) << "|" << endl;
+    cout << setw(30) << "| Tic-tac-toe:       |" << setw(40) << " |" << setw(2) << "|" << endl;
+    cout << setw(30) << "| Enter 3            |" << setw(45) << " |--| |--|" << endl;
+    cout << setw(31) << "|======================|" << setw(44) << "|--   --|" << endl;
+    cout << setw(31) << "|======================|" << setw(39) << " |" << setw(2) << "|" << endl;
+    cout << setw(70) << " |" << setw(2) << "|" << endl;
+    cout << setw(70) << " |" << setw(2) << "|" << endl;
+    cout << setw(70) << " |" << setw(2) << "|" << endl;
+    cout << setw(70) << " |" << setw(2) << "|" << endl;
+    cout << setw(70) << " |" << setw(2) << "|" << endl;
+    cout << setw(70) << " |" << setw(2) << "|" << endl;
+    cout << setw(71) << "|" << endl;
+    
+}
