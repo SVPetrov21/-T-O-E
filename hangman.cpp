49ac70d8a1d8de007de1859a46d0fd4b5c342913
BADimov21 <BADimov21@codingburgas.bg>
2022-12-11 22:57:45 +0200

    Add random words algorithm
    Co-authored-by: Bozhidar Dimov <BADimov21@codingburgas.bg>
    Co-authored-by: Gospodin Radev <GSRadev21@codingburgas.bg>

--- hangman.cpp
+++ hangman.cpp
@@ -83,9 +83,74 @@ void drawMan()
 		cout << endl;
 	}
 }
+//Main activities of the game.
+int playGame(string wordlist[])
+{
+	int exposed = 0;
+	int random = 0;
+	string word;
+	string display = word;
 
+	cout << "HANGMAN - A game made by ᒍᑌᔕT ᑕOᗪE" << endl;
+	cout << "Hello there! Welcome to hangman! You have to guess the word, you have 6 lives, and if you make a mistake 6 times you get killed. Small letters only! Good luck!" << endl;
+	cout << endl;
+	cout << "This is you. SAVE YOURSELF!" << endl;
+	cout << "---------------" << endl;
+	cout << "|             |" << endl;
+	cout << "|             O" << endl;
+	cout << "|            /|\\" << endl;
+	cout << "|            / \\" << endl;
+	cout << "|         '''''''''" << endl;
+	cout << endl;
+
+	//Random number generator.
+	srand((unsigned)time(NULL));
+	for (int i = 1; i <= 1; i++)
+	{
+		random = (rand() % 10);
+	}
+	if (random == 1)
+	{
+		word = wordlist[1];
+	}
+	else if (random == 2)
+	{
+		word = wordlist[2];
+	}
+	else if (random == 3)
+	{
+		word = wordlist[3];
+	}
+	else if (random == 4)
+	{
+		word = wordlist[4];
+	}
+	else if (random == 5)
+	{
+		word = wordlist[5];
+	}
+	else if (random == 6)
+	{
+		word = wordlist[6];
+	}
+	else if (random == 7)
+	{
+		word = wordlist[7];
+	}
+	else if (random == 8)
+	{
+		word = wordlist[8];
+	}
+	else if (random == 9)
+	{
+		word = wordlist[9];
+	}
+	display = word;
+}
 
 int main()
 {
-	drawMan();
+	string wordlist[10] = { "programming", "hangman", "spaghetti", "responsibility", "address", "operator", "ideality", "compiler", "island", "yourself" };
+	playGame(wordlist);
+	return 0;
 }
