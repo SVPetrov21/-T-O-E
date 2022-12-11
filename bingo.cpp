852201230efe75bea005a4df2413ab024e071c1b
BADimov21 <BADimov21@codingburgas.bg>
2022-12-11 22:06:12 +0200

    Add bingo card and check with numbers
    Co-authored-by: Bozhidar Dimov <BADimov21@codingburgas.bg>
    Co-authored-by: Gospodin Radev <GSRadev21@codingburgas.bg>

--- bingo.cpp
+++ bingo.cpp
@@ -15,7 +15,11 @@ void display(int matrix[5][5])
 
 int main()
 {
+    int count = 0, random = 1, counter = 0;
     int matrix[5][5];
+    srand((unsigned)time(NULL));
+    cout << "BINGO" << endl;
+    cout << "Rules: Enter your bingo card consisting of 5 rows and 5 columns. Whenever you get 5 horizontal, vertical or diagonal guessed numbers you win and get Bingo!" << endl;
     for (int i = 0; i < 5; i++)
     {
         for (int j = 0; j < 5; j++)
@@ -23,5 +27,31 @@ int main()
             cin >> matrix[i][j];
         }
     }
-    display(matrix);
+    while (count != 5)
+    {
+        for (int i = 1; i <= 1; i++)
+        {
+            random = rand() % 100;
+        }
+        cout << random;
+        counter++;
+        if (counter == 140)
+        {
+            cout << "Unlucky! Try again.";
+            break;
+        }
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                if (matrix[i][j] == random)
+                {
+                    matrix[i][j] = 0;
+                    cout << endl;
+                    cout << "Yes, you got a number!" << endl;
+                }
+            }
+        }
+        display(matrix);
+        cout << endl;
 }
